3bd82cf29308b036f168a2a44a009a78ae575ef3
hanafy0 <30327222+hanay0@users.noreply.github.com>
2018-04-26 14:23:54 +0200

    Solution PSD

---
+++ js/plugin.js
@@ -0,0 +1,29 @@
+/*global console, alert, prompt, $, document */
+
+
+//$(document).ready(function () {
+//    "use strict";
+//    
+//    $(".pricing .pricingPlans div").hover(function () {
+//       
+//        $(".pricing .pricingPlans div ul li").css({color: "#FFF"});
+//    });
+//    
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
